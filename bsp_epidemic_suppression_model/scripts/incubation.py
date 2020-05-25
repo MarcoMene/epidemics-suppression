fc058048f56d46749fb3c3d6e47721b67558c633
Andrea Maiorana <anm@bendingspoons.com>
2020-05-25 17:18:21 +0200

    feat: add average incubation time to script

--- bsp_epidemic_suppression_model/scripts/incubation.py
+++ bsp_epidemic_suppression_model/scripts/incubation.py
@@ -1,9 +1,21 @@
 from bsp_epidemic_suppression_model.utilities.model import FS
 from bsp_epidemic_suppression_model.utilities.plotting_utils import plot_functions
-from bsp_epidemic_suppression_model.utilities.functions_utils import RealRange
+from bsp_epidemic_suppression_model.utilities.functions_utils import (
+    RealRange,
+    integrate,
+)
 
 tau_max = 30
 step = 0.05
 
-print("The CDF of tau^S:")
-plot_functions([FS], RealRange(x_min=0, x_max=tau_max, step=step))
+
+plot_functions(
+    [FS],
+    RealRange(x_min=0, x_max=tau_max, step=step),
+    labels=["FS"],
+    title="The CDF of tau^S",
+)
+EtauS = integrate(
+    lambda tau: (1 - FS(tau)), 0, tau_max
+)  # Expected time of symptoms onset for symptomatics
+print("E(tauS) =", EtauS)
