0a5cc0a03a47ed529d7bc68f52bd0be4c95d15d9
Andrea Maiorana <anm@bendingspoons.com>
2020-07-28 16:15:58 +0200

    refactor: clean convolution example

--- scripts/convolution_example.py
+++ scripts/convolution_example.py
@@ -6,8 +6,8 @@ from bsp_epidemic_suppression_model.math_utilities.functions_utils import (
     RealRange,
 )
 
-f1 = lambda x: gamma_pdf(x - 10, 4, 1)
-f2 = DeltaMeasure(position=5, height=1)
+f = lambda x: gamma_pdf(x - 10, 4, 1)
+delta = DeltaMeasure(position=5, height=1)
 
-g = convolve(f1, f2, RealRange(0, 30, 1))
-plot_functions([f1, g], RealRange(0, 30, 0.1))
+g = convolve(f, delta)
+plot_functions([f, g], RealRange(0, 30, 0.1))
