cccabef0bba4b1685f3817806abfcbf694acdb54
Andrea Maiorana <anm@bendingspoons.com>
2020-08-10 18:16:33 +0200

    feat: redefine infectiousness as Weibull distribution

--- bsp_epidemic_suppression_model/math_utilities/distributions.py
+++ bsp_epidemic_suppression_model/math_utilities/distributions.py
@@ -6,21 +6,28 @@ from scipy.stats import gamma, norm
 
 
 def gamma_pdf(x: float, alpha: float, beta: float) -> float:
-    """"
+    """
     PDF of gamma distribution.
     """
     return gamma.pdf(x, a=alpha, scale=1 / beta)
 
 
 def gamma_cdf(x: float, alpha: float, beta: float) -> float:
-    """"
+    """
     CDF of gamma distribution.
     """
     return gamma.cdf(x, a=alpha, scale=1 / beta)
 
 
 def lognormal_cdf(x: float, mu: float, sigma: float) -> float:
-    """"
+    """
     CDF of log-normal distribution.
     """
     return norm.cdf((np.log(x) - mu) / sigma)
+
+
+def weibull_pdf(x: float, k: float, lambda_: float) -> float:
+    """
+    PDF of Weibull distribution with shape k and scale lambda_.
+    """
+    return k / lambda_ * (x / lambda_) ** (k - 1) * np.exp(-((x / lambda_) ** k))
--- bsp_epidemic_suppression_model/model_utilities/epidemic_data.py
+++ bsp_epidemic_suppression_model/model_utilities/epidemic_data.py
@@ -1,31 +1,38 @@
 """
 This file contains epidemic data specific of COVID-19 used in the calculations as inputs.
 Note that we normalize the "default" effective reproduction number R0 to 1, as we are only interested in the relative
-suppression. On the other hand, the breakdown of R0 in components and the shape of the infectiousness distribution beta0
-are taken from the literature, as well as the incubation period distribution.
+suppression. On the other hand, the breakdown of R0 in components and the shape of the infectiousness beta0 are taken
+from the literature, as well as the incubation period distribution.
 Here we consider a "two components model", in which the infected individuals are only divided, by illness severity, into
 symptomatic and asymptomatic.
-The source for all the numeric values are
+The source for all the numeric values is
 https://science.sciencemag.org/highwire/markup/744126/expansion?width=1000&height=500&iframe=true&postprocessors=highwire_figures%2Chighwire_math%2Chighwire_embed
-and ... # TODO Add source for alpha and beta
 """
 from typing import Tuple, List, Callable
 
 from bsp_epidemic_suppression_model.math_utilities.distributions import (
     lognormal_cdf,
     gamma_pdf,
+    weibull_pdf,
 )
 
 
 # Default effective reproduction number
 R0 = 1
-alpha = 4.865916955
-beta = 0.6487889273
+k, lambda_ = (
+    2.855,
+    5.611,
+)  # These parameters give a Weibull distribution with mean 5.00 and variance 3.61
+
+
+def rho0(tau: float) -> float:
+    """Default generation time distribution."""
+    return weibull_pdf(tau, k, lambda_)
 
 
 def beta0(tau: float) -> float:
-    """Default infectiousness distribution."""
-    return R0 * gamma_pdf(tau, alpha, beta)
+    """Default infectiousness."""
+    return R0 * rho0(tau)
 
 
 p_sym = 0.6  # Fraction of infected individuals who are symptomatic.
@@ -46,13 +53,13 @@ assert R0 == p_sym * R0_sym + p_asy * R0_asy
 
 
 def beta0_asy(tau: float):
-    """Default infectiousness distribution for asymptomatic individuals."""
-    return R0_asy * gamma_pdf(tau, alpha, beta)
+    """Default infectiousness for asymptomatic individuals."""
+    return R0_asy * rho0(tau)
 
 
 def beta0_sym(tau: float):
-    """Default infectiousness distribution for symptomatic individuals."""
-    return R0_sym * gamma_pdf(tau, alpha, beta)
+    """Default infectiousness for symptomatic individuals."""
+    return R0_sym * rho0(tau)
 
 
 def make_scenario_parameters_for_asymptomatics_symptomatics_model() -> Tuple[
